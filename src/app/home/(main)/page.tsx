ec35d73f4cc40ca85bd18ab8b50d5774d844b5df
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-01-13 13:21:11 +0900

    구조

---
+++ src/app/home/(main)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from '@/components/homepage/HomePage';
+
+// 클라이언트 컴포넌트를 사용하는 경우 "use client" 추가
+const Home = () => {
+    return (
+        <HomePage />
+    );
+};
+
+export default Home;
